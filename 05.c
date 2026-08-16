b9c305074039f6c5676ef8b6bc6300e8f8f549ad
ISHPREET2007 <kaberwalishpreet@gmail.com>
2026-08-16 17:53:59 +0530

    this is a commit

---
+++ 05.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    // here learn about relational operators,these are used in if statements.
+    int a = 1, b = 0;
+
+    if (a==b)
+    {
+        printf("Zero and One are equal.\n");
+    }
+    else
+    {
+        printf("Zero and one are not equal.\n");
+    }
+    if (a!=b)
+    {
+        printf("Zero and one are not equal.\n");
+    }
+    if (a>=b)
+    {
+        printf("True.\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+    if (a<=b)
+    {
+        printf("True.\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+    return 0;
+    // <,> already done.
+}
---
+++ fourth.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int age = 24;
+    if (age < 18)
+    {
+        printf("You cant drive.");
+    }
+    else
+    {
+        printf("you can drive.");
+    }
+    return 0;
+    // by this i learnt about if-else statements.
+}
